18113e7e6db58a21ba427d81323e7013e3012c93
aimanshakeel <aimanshakeel1008@gmail.com>
2024-06-25 18:27:39 +0530

    second commit

--- app1/main.py
+++ app1/main.py
@@ -1,1 +1,12 @@
-print("Add a new Todo:")
+#print("Add a new Todo:")
+
+prompt = "Enter a todo:"
+user_response1 = input(prompt)
+user_response2 = input(prompt)
+user_response3 = input(prompt)
+
+todos = [user_response1, user_response2, user_response3]
+print(todos)
+
+print(type(user_response1))
+print(type(todos))
